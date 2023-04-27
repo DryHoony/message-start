c4295349515e26f814ca614b3f36abc41d948fa1
Hoony <xognsdl710@naver.com>
2023-04-28 02:05:50 +0900

    2023.04.28 5. 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 3.) 메시지, 국제화 - 변경내용 확인

--- ItemServiceApplication.java
+++ ItemServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ItemServiceApplication {
+	// 변경내용
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
